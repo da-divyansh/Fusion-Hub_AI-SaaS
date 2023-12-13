109c24456aca4870878048ea7b9b6caaa171264f
Divyansh <maddhesiyadivyansh@gmail.com>
2023-12-13 12:59:39 +0530

    Added Video generation page

---
+++ app/api/video/route.ts
@@ -0,0 +1,39 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import Replicate from "replicate";
+
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_KEY
+})
+
+// Main API route handler
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { prompt } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 404 });
+    }
+
+    if (!prompt) {
+      return new NextResponse("Music prompt is required", { status: 400 });
+    }
+
+    const response = await replicate.run(
+      "anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
+      {
+        input: {
+          prompt
+        }
+      }
+    );
+    return NextResponse.json(response);
+    
+  } catch (error) {
+    console.log("Video_ Error: ", error);
+    
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
