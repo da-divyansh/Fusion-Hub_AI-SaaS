f4ad392a7a707d5bdfb0fd2c6d9ab2afec9c825f
Divyansh <maddhesiyadivyansh@gmail.com>
2023-12-13 12:29:37 +0530

    Added API to music generation page

--- app/(dashboard)/(routes)/music/page.tsx
+++ app/(dashboard)/(routes)/music/page.tsx
@@ -95,9 +95,11 @@ import { Loader } from '@/components/loader';
                 <Empty label='No music generated.' />            
               </div>
             )}
-            <div>
-              Music will be generated here.
-            </div>
+            {music && (
+              <audio controls className="w-full mt-8">
+                <source src= {music} />
+              </audio>
+            )}
           </div>
         </div>
       </div>
